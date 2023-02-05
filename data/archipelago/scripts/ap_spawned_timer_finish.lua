700bd8bc6451c36584d8ba90164e16b5b8260b1a
Scipio Wright <scipiowright@gmail.com>
2023-02-05 15:38:33 -0500

    Revised spawned timer to use a simple command instead of spawning an entity and waiting for it to time out. Also actually implemented the spawn timer so that potions and stuff don't deliver within the first half second after spawning.

--- data/archipelago/scripts/ap_spawned_timer_finish.lua
+++ data/archipelago/scripts/ap_spawned_timer_finish.lua
@@ -1,10 +1,11 @@
 dofile_once("data/scripts/lib/utilities.lua")
 dofile_once("data/archipelago/scripts/ap_utils.lua")
 
-local LOAD_KEY = "timer_first_load_done"
 -- just putting this here for now in case I need it, can remove the flag later if it's not useful
 GameAddFlagRun("spawned_timer_finished")
-if GlobalsGetValue(LOAD_KEY) ~= "1" then
-    GlobalsSetValue(LOAD_KEY, "1")
+print("spawned timer finish script started")
+if GlobalsGetValue("timer_first_load_done") ~= "1" then
+    GlobalsSetValue("timer_first_load_done", "1")
     fully_heal() -- since on spawn, you get your extra max hp from previous runs but don't get healed for it
+    print("spawned timer did it's job I think")
 end
