805dce4737d3c796a944c377f8a4bd5cb577b938
Scipio Wright <scipiowright@gmail.com>
2023-02-05 15:50:25 -0500

    Changed dofile to dofile_once for game_helpers, removed a bunch of prints that aren't needed anymore.

--- data/archipelago/scripts/ap_orb_init_random.lua
+++ data/archipelago/scripts/ap_orb_init_random.lua
@@ -1,4 +1,4 @@
-dofile( "data/scripts/game_helpers.lua" )
+dofile_once( "data/scripts/game_helpers.lua" )
 dofile_once("data/scripts/lib/utilities.lua")
 dofile_once("data/archipelago/scripts/ap_utils.lua")
 local AP = dofile("data/archipelago/scripts/constants.lua")
@@ -17,24 +17,11 @@ for _, comp_id in pairs(orbcomp) do
 	EntityRemoveComponent(entity_id, comp_id)
 end
 
-print("orb_id is " .. orb_id)
-
 local location = Globals.LocationScouts:get_key(orb_id + Constants.FIRST_ORB_LOCATION_ID)
-print("location ID shoudl be " .. Constants.FIRST_ORB_LOCATION_ID)
 local flags = location.item_flags
 
 -- there's no AP.ITEM_FLAG_JUNK, so the default appearance is junk instead
 local orb_file = "ap_orb_junk"
-
-print("ap_locationscouts_data is " .. GlobalsGetValue("AP_LOCATIONSCOUTS_DATA"))
-
-print("ap.item_flags below")
-print(location)
-print(bit.band(flags, AP.ITEM_FLAG_TRAP))
-print(bit.band(flags, AP.ITEM_FLAG_USEFUL))
-print(bit.band(flags, AP.ITEM_FLAG_PROGRESSION))
-
-print(GlobalsGetValue("AP_LOCATIONSCOUTS_DATA"))
 
 if bit.band(flags, AP.ITEM_FLAG_TRAP) ~= 0 then
 	orb_file = "ap_orb_trap_" .. tostring(Random(1,3))
