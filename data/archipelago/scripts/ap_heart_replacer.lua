e14bb0f0077de5bf758c12ff5605d2b8dde05a72
Scipio Wright <scipiowright@gmail.com>
2023-02-05 16:00:54 -0500

    Wrapped the heart replacer and ap orb init random in commands to localize stuff per what Heinermann said

--- data/archipelago/scripts/ap_heart_replacer.lua
+++ data/archipelago/scripts/ap_heart_replacer.lua
@@ -1,18 +1,23 @@
--- sets a function for the old version of spawn_heart
-local ap_old_spawn_heart = spawn_heart
 
-local remaining_checks_in_biome = 5  -- rename, make the value dependent on an options slider probably
+local function APHeartReplacer()
+    -- sets a function for the old version of spawn_heart
+    local ap_old_spawn_heart = spawn_heart
 
-local function ap_replace_heart(x, y)
-    EntityLoad("data/archipelago/entities/items/pickup/ap_chest_random.xml", x, y)
-    -- todo: change this item, make it spawn a chest or heart if remaining_checks_in_biome == 0
-    -- todo: make something actualyl increment remaining_checks_in_biome
-end
+    local remaining_checks_in_biome = 5  -- rename, make the value dependent on an options slider probably
 
-spawn_heart = function(x, y)
-    if remaining_checks_in_biome == 0 then
-        ap_old_spawn_heart(x, y)
-    else
-        ap_replace_heart(x, y)
+    local function ap_replace_heart(x, y)
+        EntityLoad("data/archipelago/entities/items/pickup/ap_chest_random.xml", x, y)
+        -- todo: change this item, make it spawn a chest or heart if remaining_checks_in_biome == 0
+        -- todo: make something actualyl increment remaining_checks_in_biome
+    end
+
+    spawn_heart = function(x, y)
+        if remaining_checks_in_biome == 0 then
+            ap_old_spawn_heart(x, y)
+        else
+            ap_replace_heart(x, y)
+        end
     end
 end
+
+APHeartReplacer()
