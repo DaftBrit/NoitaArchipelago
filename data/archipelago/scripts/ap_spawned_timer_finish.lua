cc679e4dedd99dc24312ac03b4a49ed9abbd7d30
Scipio Wright <scipiowright@gmail.com>
2023-02-11 10:10:25 -0500

    Renaming spawned timers to add the ap_ prefix

--- data/archipelago/scripts/ap_spawned_timer_finish.lua
+++ data/archipelago/scripts/ap_spawned_timer_finish.lua
@@ -2,10 +2,10 @@ dofile_once("data/scripts/lib/utilities.lua")
 dofile_once("data/archipelago/scripts/ap_utils.lua")
 
 -- just putting this here for now in case I need it, can remove the flag later if it's not useful
-GameAddFlagRun("spawned_timer_finished")
+GameAddFlagRun("ap_spawned_timer_finished")
 print("spawned timer finish script started")
-if GlobalsGetValue("timer_first_load_done") ~= "1" then
-    GlobalsSetValue("timer_first_load_done", "1")
+if GlobalsGetValue("ap_timer_first_load_done") ~= "1" then
+    GlobalsSetValue("ap_timer_first_load_done", "1")
     fully_heal() -- since on spawn, you get your extra max hp from previous runs but don't get healed for it
     print("spawned timer did it's job I think")
 end
