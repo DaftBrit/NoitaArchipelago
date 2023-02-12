592b26f790d95e79000b1b12016b49f76a68d741
Scipio Wright <scipiowright@gmail.com>
2023-02-12 12:03:32 -0500

    Refactor for the orb images. Instead of child entities, we now just add new sprite components to the orb instead -- much simpler this way, less physics weirdness.

--- data/archipelago/scripts/ap_orb_init_random.lua
+++ data/archipelago/scripts/ap_orb_init_random.lua
@@ -22,47 +22,33 @@ local function APOrbInitRandom()
 	local location = Globals.LocationScouts:get_key(orb_id + Constants.FIRST_ORB_LOCATION_ID)
 	local flags = location.item_flags
 
-	-- there's no AP.ITEM_FLAG_JUNK, so the default appearance is junk instead
-	--local orb_file = "ap_orb_junk"
-
-	--if bit.band(flags, AP.ITEM_FLAG_TRAP) ~= 0 then
-	--	orb_file = "ap_orb_trap_" .. tostring(Random(1,3))
-	--elseif bit.band(flags, AP.ITEM_FLAG_USEFUL) ~= 0 then
-	--	orb_file = "ap_orb_useful"
-	--elseif bit.band(flags, AP.ITEM_FLAG_PROGRESSION) ~= 0 then
-	--	orb_file = "ap_orb_progression"
-	--end
-	--
-	--for _, comp_id in pairs(spritecomp) do
-	--	ComponentSetValue2(comp_id, "image_file", "data/archipelago/entities/items/orbs/" .. orb_file .. ".xml")
-	--end
-
-	--local orb_file = "ap_orb_useful"
-	--
-	--for _, comp_id in pairs(spritecomp) do
-	--	ComponentSetValue2(comp_id, "image_file", "data/archipelago/entities/items/orbs/" .. orb_file .. ".xml")
-	--end
-
 	for _, comp_id in pairs(spritecomp) do
 		ComponentSetValue2(comp_id, "image_file", "data/items_gfx/orbs/orb.xml")
 	end
 
-	local game_overlay_icon = "ap_logo_orb"
-	local overlay_file = "filler_icon"
+	local orb_image = "ap_logo_orb"
+	local check_type_icon = "filler_icon"
 
 	if bit.band(flags, AP.ITEM_FLAG_TRAP) ~= 0 then
-		overlay_file = "trap_icon"
+		check_type_icon = "empty_icon"
+		orb_image = "ap_logo_orb_trap_" .. tostring(Random(1,3))
 	elseif bit.band(flags, AP.ITEM_FLAG_USEFUL) ~= 0 then
-		print("skip useful for overlay")
+		check_type_icon = "empty_icon"
 	elseif bit.band(flags, AP.ITEM_FLAG_PROGRESSION) ~= 0 then
-		overlay_file = "progression_icon"
+		check_type_icon = "progression_icon"
 	end
 
-	local game_overlay_entity = EntityLoad("data/archipelago/entities/items/overlay/" .. game_overlay_icon .. ".xml", pos_x, pos_y)
-	EntityAddChild(entity_id, game_overlay_entity)
+	EntityAddComponent2(entity_id, "SpriteComponent", {
+		image_file = "data/archipelago/entities/items/icons/" .. orb_image .. ".png",
+		offset_x = 7,
+		offset_y = 20,
+	})
 
-	local overlay_entity = EntityLoad("data/archipelago/entities/items/overlay/" .. overlay_file .. ".xml", pos_x, pos_y)
-	EntityAddChild(entity_id, overlay_entity)
+	EntityAddComponent2(entity_id, "SpriteComponent", {
+		image_file = "data/archipelago/entities/items/icons/" .. check_type_icon .. ".png",
+		offset_x = 0,
+		offset_y = 10,
+	})
 
 	--this variable just stores the original orb_id elsewhere, not sure if it'll be needed later?
 	addNewInternalVariable(entity_id, "OriginalID", "value_int", orb_id)
