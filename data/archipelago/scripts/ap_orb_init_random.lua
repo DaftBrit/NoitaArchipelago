bd1b323446dc1805ef474591a70072a01f923b09
Scipio Wright <scipiowright@gmail.com>
2023-02-25 19:53:29 -0500

    Fixed ap_orb_init_random so that you can pick up orbs that are not location checks, and it doesn't modify their art.

--- data/archipelago/scripts/ap_orb_init_random.lua
+++ data/archipelago/scripts/ap_orb_init_random.lua
@@ -21,15 +21,15 @@ local function APOrbInitRandom()
 
 	local location = Globals.LocationScouts:get_key(orb_id + Constants.FIRST_ORB_LOCATION_ID)
 	local flags = location.item_flags
-
-	for _, comp_id in pairs(spritecomp) do
-		ComponentSetValue2(comp_id, "image_file", "data/items_gfx/orbs/orb.xml")
-	end
-
 	local orb_image = "ap_logo_orb"
 	local check_type_icon = "filler_icon"
 
-	if bit.band(flags, AP.ITEM_FLAG_TRAP) ~= 0 then
+	--this variable just stores the original orb_id elsewhere
+	addNewInternalVariable(entity_id, "OriginalID", "value_int", orb_id)
+
+	if flags == nil then
+		print("this orb is not a location check")
+	elseif bit.band(flags, AP.ITEM_FLAG_TRAP) ~= 0 then
 		check_type_icon = "empty_icon"
 		orb_image = "ap_logo_orb_trap_" .. tostring(Random(1,3))
 	elseif bit.band(flags, AP.ITEM_FLAG_USEFUL) ~= 0 then
@@ -38,20 +38,23 @@ local function APOrbInitRandom()
 		check_type_icon = "progression_icon"
 	end
 
-	EntityAddComponent2(entity_id, "SpriteComponent", {
-		image_file = "data/archipelago/entities/items/icons/" .. orb_image .. ".png",
-		offset_x = 7,
-		offset_y = 20,
-	})
+	if flags ~= nil then
+		for _, comp_id in pairs(spritecomp) do
+			ComponentSetValue2(comp_id, "image_file", "data/items_gfx/orbs/orb.xml")
+		end
 
-	EntityAddComponent2(entity_id, "SpriteComponent", {
-		image_file = "data/archipelago/entities/items/icons/" .. check_type_icon .. ".png",
-		offset_x = 0,
-		offset_y = 10,
-	})
+		EntityAddComponent2(entity_id, "SpriteComponent", {
+			image_file = "data/archipelago/entities/items/icons/" .. orb_image .. ".png",
+			offset_x = 7,
+			offset_y = 20,
+		})
 
-	--this variable just stores the original orb_id elsewhere, not sure if it'll be needed later?
-	addNewInternalVariable(entity_id, "OriginalID", "value_int", orb_id)
+		EntityAddComponent2(entity_id, "SpriteComponent", {
+			image_file = "data/archipelago/entities/items/icons/" .. check_type_icon .. ".png",
+			offset_x = 0,
+			offset_y = 10,
+		})
+	end
 end
 
 APOrbInitRandom()
