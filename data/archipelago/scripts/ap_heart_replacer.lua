bc16ffc677f118b51f82fd22dab62a9ef4bf0178
Scipio Wright <scipiowright@gmail.com>
2023-03-10 22:11:37 -0500

    Heart replacer now fully works, and hidden chests are now implemented properly. Probably going to still edit it a little bit to mess with rates though.

--- data/archipelago/scripts/ap_heart_replacer.lua
+++ data/archipelago/scripts/ap_heart_replacer.lua
@@ -1,38 +1,46 @@
+local Biomes = dofile("data/archipelago/scripts/ap_biome_mapping.lua")
+local Globals = dofile("data/archipelago/scripts/globals.lua")
 
 local function APHeartReplacer()
     -- sets a function for the old version of spawn_heart
     local ap_old_spawn_heart = spawn_heart
 
-    local remaining_checks_in_biome = 5  -- rename, make the value dependent on an options slider probably
-
     local function ap_replace_heart(x, y)
-        local ap_chest_id = EntityLoad("data/archipelago/entities/items/pickup/ap_chest_random.xml", x, y)
         local biome_name = BiomeMapGetName(x, y)
-        EntityAddComponent(ap_chest_id, "VariableStorageComponent",
-        {
-        name = "biome_name",
-        value_string = biome_name,
-        })
-        -- todo: change this item, make it spawn a chest or heart if remaining_checks_in_biome == 0
-        -- todo: make something actually increment remaining_checks_in_biome
-        -- should we make the chests have the corresponding item type icon?
-        -- that would mean each chest would need to specifically correspond to the check in it
-        -- and currently it just spawns the next flag in the list
+        -- check if the biome has checks left, if not then just spawn a chest/heart as normal
+        for _, biome_data in pairs(Biomes) do
+            if biome_name == biome_data.name then
+                for i = biome_data.first_hc, biome_data.first_hc + 19 do
+                    if Globals.MissingLocationsSet:has_key(i) then
+                        -- spawn the chest, set ap_chest_id equal to its entity ID
+                        local ap_chest_id = EntityLoad("data/archipelago/entities/items/pickup/ap_chest_random.xml", x, y)
+                        EntityAddComponent(ap_chest_id, "VariableStorageComponent",
+                                {
+                                    name = "biome_name",
+                                    value_string = biome_name,
+                                })
 
-        -- if the chest manages to spawn outside of a biome, just kill it and spawn a normal heart or chest instead
-        if biome_name == "_EMPTY_" then
-            EntityKill(ap_chest_id)
-            ap_old_spawn_heart(x, y)
+                        -- if the chest manages to spawn outside of a biome, just kill it and spawn a normal heart or chest instead
+                        if biome_name == "_EMPTY_" then
+                            EntityKill(ap_chest_id)
+                            ap_old_spawn_heart(x, y)
+                        end
+                        break
+
+                        -- do nothing, continue with the script
+                    else
+                        ap_old_spawn_heart(x, y)
+                        break
+                    end
+                end
+            end
         end
+    end
 
-        end
 
+    -- this makes the spawn_heart the game calls for redirect to ap_replace_heart
     spawn_heart = function(x, y)
-        if remaining_checks_in_biome == 0 then
-            ap_old_spawn_heart(x, y)
-        else
-            ap_replace_heart(x, y)
-        end
+        ap_replace_heart(x, y)
     end
 end
 
