d4dc2746f1f84c8ad4cd2ea4cc6bcc10da826b3f
Scipio Wright <scipiowright@gmail.com>
2023-03-12 20:43:15 -0400

    Wand pedestals are now checks

--- data/archipelago/scripts/ap_pedestal_replacer.lua
+++ data/archipelago/scripts/ap_pedestal_replacer.lua
@@ -1,10 +1,33 @@
--- todo: finish this script
-local function PedestalWandReplacer()
+local Biomes = dofile("data/archipelago/scripts/ap_biome_mapping.lua")
+local Globals = dofile("data/archipelago/scripts/globals.lua")
 
-    local old_spawn_wands = spawn_wands
+local function PedestalWandReplacer()
+    local ap_old_spawn_wands = spawn_wands
 
     local function ap_replace_pedestals(x, y)
-        old_spawn_wands(x, y)
+        local biome_name = BiomeMapGetName(x, y)
+        local has_spawned = false
+        -- check if the biome has checks left, if not then just spawn a chest/heart as normal
+        if Biomes[biome_name] ~= nil then
+            local biome_data = Biomes[biome_name]
+            for i = biome_data.first_ped, biome_data.first_ped + 19 do
+                if Globals.MissingLocationsSet:has_key(i) then
+                    -- spawn the chest, set ap_chest_id equal to its entity ID
+                    local ap_chest_id = EntityLoad("data/archipelago/entities/items/pickup/ap_pedestal_random.xml", x, y)
+                    has_spawned = true
+                    EntityAddComponent(ap_chest_id, "VariableStorageComponent",
+                            {
+                                name = "biome_name",
+                                value_string = biome_name,
+                            })
+                    break
+                end
+            end
+        end
+        -- chest spawned in non-applicable biome or no more chests for that biome, spawn heart/chest normally
+        if has_spawned ~= true then
+            ap_old_spawn_wands(x, y)
+        end
     end
 
     spawn_wands = function(x, y)
---
+++ data/archipelago/scripts/items/ap_pedestal_random.lua
@@ -0,0 +1,39 @@
+dofile_once("data/scripts/lib/utilities.lua")
+dofile_once("data/scripts/gun/gun_actions.lua")
+dofile_once("data/scripts/game_helpers.lua")
+local Biomes = dofile("data/archipelago/scripts/ap_biome_mapping.lua")
+local Globals = dofile("data/archipelago/scripts/globals.lua")
+dofile_once("data/archipelago/scripts/item_utils.lua")
+
+
+function on_open(entity_item)
+	local biome_comp_id = EntityGetFirstComponent(entity_item, "VariableStorageComponent")
+	local biome_name = ComponentGetValue2(biome_comp_id, "value_string")
+	if Biomes[biome_name] ~= nil then
+		local biome_data = Biomes[biome_name]
+		for i = biome_data.first_ped, biome_data.first_ped + 19 do
+			if Globals.MissingLocationsSet:has_key(i) then
+				Globals.LocationUnlockQueue:append(i)
+				Globals.MissingLocationsSet:remove_key(i)
+				-- for some reason, if the below is included it double spawns items? idk why
+				--local location = Globals.LocationScouts:get_key(i)
+				--if location == nil then
+				--	Log.Error("ap_chest_random failed to retrieve info from cache")
+				--end
+				--local item_id = location.item_id
+				--if location.is_our_item then
+				--	SpawnItem(item_id, true)
+				--end
+				break
+			end
+		end
+	end
+	EntityLoad("data/entities/particles/image_emitters/chest_effect.xml", x, y)
+end
+
+
+function item_pickup( entity_item, entity_who_picked, name )
+	on_open( entity_item )
+
+	EntityKill( entity_item )
+end
