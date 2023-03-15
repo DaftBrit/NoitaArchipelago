89a46498ec959cb649ca8752601071031b466c2f
Scipio Wright <lightdemonjoe4@gmail.com>
2023-03-15 09:43:48 -0400

    Change to pedestals so that the order is random
    Did this in-browser, and so didn't really check to make sure it actually works. Will need to check it in an ide later

--- data/archipelago/scripts/items/ap_pedestal_random.lua
+++ data/archipelago/scripts/items/ap_pedestal_random.lua
@@ -9,30 +9,31 @@ dofile_once("data/archipelago/scripts/item_utils.lua")
 function on_open(entity_item)
 	local biome_comp_id = EntityGetFirstComponent(entity_item, "VariableStorageComponent")
 	local biome_name = ComponentGetValue2(biome_comp_id, "value_string")
-	local item_sent = nil
+	local missing_locations = []
 	if Biomes[biome_name] ~= nil then
 		local biome_data = Biomes[biome_name]
 		for i = biome_data.first_ped, biome_data.first_ped + 19 do
 			if Globals.MissingLocationsSet:has_key(i) then
-				Globals.LocationUnlockQueue:append(i)
-				Globals.MissingLocationsSet:remove_key(i)
-				item_sent = true
-				-- for some reason, if the below is included it double spawns items? idk why
-				--local location = Globals.LocationScouts:get_key(i)
-				--if location == nil then
-				--	Log.Error("ap_chest_random failed to retrieve info from cache")
-				--end
-				--local item_id = location.item_id
-				--if location.is_our_item then
-				--	SpawnItem(item_id, true)
-				--end
-				break
+				table.insert(missing_locations, i)
 			end
 		end
-		if item_sent ~= true then
-			-- if you found all of your checks for that biome but find another ap pedestal from the biome
-			-- todo: spawn random item
-		end
+	end
+	local location = missing_locations[math.random(#missing_locations)]
+	if location ~= 0 then
+		Globals.LocationUnlockQueue:append(location)
+		Globals.MissingLocationsSet:remove_key(location)
+		-- for some reason, if the below is included it double spawns items? idk why
+		--local location = Globals.LocationScouts:get_key(i)
+		--if location == nil then
+		--	Log.Error("ap_chest_random failed to retrieve info from cache")
+		--end
+		--local item_id = location.item_id
+		--if location.is_our_item then
+		--	SpawnItem(item_id, true)
+		--end
+	else 			
+		-- if you found all of your checks for that biome but find another ap pedestal from the biome
+		-- todo: spawn random item
 	end
 	EntityLoad("data/entities/particles/image_emitters/chest_effect.xml", x, y)
 end
@@ -40,6 +41,5 @@ end
 
 function item_pickup( entity_item, entity_who_picked, name )
 	on_open( entity_item )
-
 	EntityKill( entity_item )
 end
