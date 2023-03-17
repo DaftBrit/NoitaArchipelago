c40cc1121d564378109739db69fecef2f3fc5247
Scipio Wright <scipiowright@gmail.com>
2023-03-16 22:03:24 -0400

    Now replaces potion pedestals Spawns items when picking up an existing ap pedestal item after you've already picked up the last check in that biome

--- data/archipelago/scripts/ap_pedestal_replacer.lua
+++ data/archipelago/scripts/ap_pedestal_replacer.lua
@@ -4,6 +4,8 @@ dofile_once("data/archipelago/scripts/ap_utils.lua")
 
 local function PedestalWandReplacer()
     local ap_old_spawn_wands = spawn_wands
+    local ap_old_spawn_potions = spawn_potions
+    local replaced_pedestal = ""
 
     local function ap_replace_pedestals(x, y)
         local biome_name = BiomeMapGetName(x, y)
@@ -13,21 +15,34 @@ local function PedestalWandReplacer()
             local biome_data = Biomes[biome_name]
             for i = biome_data.first_ped, biome_data.first_ped + 19 do
                 if Globals.MissingLocationsSet:has_key(i) then
-                    -- spawn the chest, set ap_chest_id equal to its entity ID
-                    local ap_chest_id = EntityLoad("data/archipelago/entities/items/pickup/ap_pedestal_random.xml", x, y)
+                    -- spawn the pedestal item, tell it its ID
+                    local ap_pedestal_id = EntityLoad("data/archipelago/entities/items/pickup/ap_pedestal_random.xml", x, y)
                     has_spawned = true
-                    addNewInternalVariable(ap_chest_id, "biome_name", "value_string", biome_name)
+                    addNewInternalVariable(ap_pedestal_id, "biome_name", "value_string", biome_name)
+                    addNewInternalVariable(ap_pedestal_id, "pedestal_type", "value_string", replaced_pedestal)
                     break
                 end
             end
         end
         -- chest spawned in non-applicable biome or no more chests for that biome, spawn heart/chest normally
         if has_spawned ~= true then
-            ap_old_spawn_wands(x, y)
+            if replaced_pedestal == "wand" then
+                ap_old_spawn_wands(x, y)
+            elseif replaced_pedestal == "potion" then
+                ap_old_spawn_potions(x, y)
+            else
+                print("error in ap_pedestal_replacer.lua in going back to the old spawn function")
+            end
         end
     end
 
     spawn_wands = function(x, y)
+        replaced_pedestal = "wand"
+        ap_replace_pedestals(x, y)
+    end
+
+    spawn_potions = function(x, y)
+        replaced_pedestal = "potion"
         ap_replace_pedestals(x, y)
     end
 end
--- data/archipelago/scripts/items/ap_pedestal_random.lua
+++ data/archipelago/scripts/items/ap_pedestal_random.lua
@@ -1,9 +1,8 @@
-dofile_once("data/scripts/lib/utilities.lua")
-dofile_once("data/scripts/gun/gun_actions.lua")
-dofile_once("data/scripts/game_helpers.lua")
 local Biomes = dofile("data/archipelago/scripts/ap_biome_mapping.lua")
 local Globals = dofile("data/archipelago/scripts/globals.lua")
 dofile_once("data/archipelago/scripts/item_utils.lua")
+dofile_once("data/scripts/item_spawnlists.lua")
+dofile_once("data/scripts/director_helpers.lua")
 
 
 function on_open(entity_item)
@@ -19,7 +18,7 @@ function on_open(entity_item)
 		end
 	end
 	local location = missing_locations[math.random(#missing_locations)]
-	if location ~= 0 then
+	if location ~= nil then
 		Globals.LocationUnlockQueue:append(location)
 		Globals.MissingLocationsSet:remove_key(location)
 		-- for some reason, if the below is included it double spawns items? idk why
@@ -32,8 +31,17 @@ function on_open(entity_item)
 		--	SpawnItem(item_id, true)
 		--end
 	else 			
-		-- if you found all of your checks for that biome but find another ap pedestal from the biome
-		-- todo: spawn random item
+		-- if you found all of your checks for that biome but find another ap pedestal that already spawned
+		local ped_x, ped_y = EntityGetTransform(entity_item)
+		local pedestal_type = getInternalVariableValue(entity_item, "pedestal_type", "value_string")
+		if pedestal_type == "wand" then
+			-- todo: find a way to make it spawn a wand from the g_items list for that biome, or random wand
+			spawn_from_list("potion_spawnlist", ped_x, ped_y)
+		elseif pedestal_type == "potion" then
+			spawn_from_list("potion_spawnlist", ped_x, ped_y)
+		else
+			print("error spawning extra pedestal location items")
+		end
 	end
 	EntityLoad("data/entities/particles/image_emitters/chest_effect.xml", x, y)
 end
