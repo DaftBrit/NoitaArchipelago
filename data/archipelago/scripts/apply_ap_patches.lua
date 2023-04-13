9747851c36e90b85e3c85974e7f1d49a0789ad46
Adam Heinermann <aheinerm@gmail.com>
2023-04-13 16:54:11 -0700

    Move patches to subdirectory

--- data/archipelago/scripts/apply_ap_patches.lua
+++ data/archipelago/scripts/apply_ap_patches.lua
@@ -5,11 +5,11 @@ local translations = ModTextFileGetContent(TRANSLATIONS_FILE) .. ModTextFileGetC
 ModTextFileSetContent(TRANSLATIONS_FILE, translations)
 
 -- SCRIPT EXTENSIONS
-ModLuaFileAppend("data/scripts/biomes/temple_altar.lua", "data/archipelago/scripts/extend_temple_altar.lua")
-ModLuaFileAppend("data/scripts/biomes/boss_arena.lua", "data/archipelago/scripts/extend_temple_altar.lua")
-ModLuaFileAppend("data/scripts/biomes/snowcastle_cavern.lua", "data/archipelago/scripts/extend_snowcastle_cavern.lua")
-ModLuaFileAppend("data/scripts/perks/perk_list.lua", "data/archipelago/scripts/ap_extend_perk_list.lua")
-ModLuaFileAppend("data/entities/animals/boss_centipede/ending/sampo_start_ending_sequence.lua", "data/archipelago/scripts/ap_extend_ending.lua")
+ModLuaFileAppend("data/scripts/biomes/temple_altar.lua", "data/archipelago/scripts/patches/extend_temple_altar.lua")
+ModLuaFileAppend("data/scripts/biomes/boss_arena.lua", "data/archipelago/scripts/patches/extend_temple_altar.lua")
+ModLuaFileAppend("data/scripts/biomes/snowcastle_cavern.lua", "data/archipelago/scripts/patches/extend_snowcastle_cavern.lua")
+ModLuaFileAppend("data/scripts/perks/perk_list.lua", "data/archipelago/scripts/patches/ap_extend_perk_list.lua")
+ModLuaFileAppend("data/entities/animals/boss_centipede/ending/sampo_start_ending_sequence.lua", "data/archipelago/scripts/patches/ap_extend_ending.lua")
 
 ModLuaFileAppend("data/entities/animals/boss_alchemist/death.lua", "data/archipelago/scripts/bosses/ap_alchemist_death.lua")
 ModLuaFileAppend("data/scripts/animals/boss_dragon_death.lua", "data/archipelago/scripts/bosses/ap_dragon_death.lua")
@@ -24,8 +24,8 @@ ModLuaFileAppend("data/entities/animals/boss_ghost/death.lua", "data/archipelago
 ModLuaFileAppend("data/entities/animals/boss_pit/boss_pit_death.lua", "data/archipelago/scripts/bosses/ap_squidward_death.lua")
 ModLuaFileAppend("data/entities/animals/boss_gate/gate_monster_death.lua", "data/archipelago/scripts/bosses/ap_triangle_death.lua")
 
-ModLuaFileAppend("data/scripts/items/orb_init.lua", "data/archipelago/scripts/ap_orb_init_random.lua")
-ModLuaFileAppend("data/scripts/items/orb_pickup.lua", "data/archipelago/scripts/ap_orb_pickup_random.lua")
+ModLuaFileAppend("data/scripts/items/orb_init.lua", "data/archipelago/scripts/patches/ap_orb_init_random.lua")
+ModLuaFileAppend("data/scripts/items/orb_pickup.lua", "data/archipelago/scripts/patches/ap_orb_pickup_random.lua")
 
-ModLuaFileAppend("data/scripts/biome_scripts.lua", "data/archipelago/scripts/ap_heart_replacer.lua")
-ModLuaFileAppend("data/scripts/biome_scripts.lua", "data/archipelago/scripts/ap_pedestal_replacer.lua")
+ModLuaFileAppend("data/scripts/biome_scripts.lua", "data/archipelago/scripts/patches/ap_heart_replacer.lua")
+ModLuaFileAppend("data/scripts/biome_scripts.lua", "data/archipelago/scripts/patches/ap_pedestal_replacer.lua")
