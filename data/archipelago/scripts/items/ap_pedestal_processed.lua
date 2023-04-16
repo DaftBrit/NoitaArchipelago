f384505626d08ba371549cb874f3dbd82e6f48da
Adam Heinermann <aheinerm@gmail.com>
2023-04-15 17:02:39 -0700

    #92 crank AP pedestals to 100%, refactor a little bit

--- data/archipelago/scripts/items/ap_pedestal_processed.lua
+++ data/archipelago/scripts/items/ap_pedestal_processed.lua
@@ -5,6 +5,6 @@ dofile_once("data/archipelago/scripts/ap_utils.lua")
 
 -- Called when the entity is picked up
 function item_pickup(entity_item, entity_who_picked, name)
-	local location_id = getInternalVariableValue(entity_item, "location", "value_int")
+	local location_id = getInternalVariableValue(entity_item, "ap_location_id", "value_int")
 	Globals.LocationUnlockQueue:append(location_id)
 end
