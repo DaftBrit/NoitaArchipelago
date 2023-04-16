dfa66ef302f133dac9d8500253c9a44bd6a5419a
Scipio Wright <scipiowright@gmail.com>
2023-04-15 21:16:46 -0400

    Fix for sampo lua comp replacement

--- data/archipelago/scripts/patches/ap_extend_sampo_pickup.lua
+++ data/archipelago/scripts/patches/ap_extend_sampo_pickup.lua
@@ -2,7 +2,10 @@ local function ap_swap_sampo_script()
 	local entity_id = GetUpdatedEntityID()
 	local lua_comps = EntityGetComponent(entity_id, "LuaComponent", "enabled_in_world")
 	for _, comp in pairs(lua_comps) do
-		ComponentSetValue2(comp, "script_source_file", "data/archipelago/scripts/patches/ap_extend_ending.lua")
+		local script = ComponentGetValue2(comp, "script_source_file")
+		if script == "data/entities/animals/boss_centipede/ending/sampo_start_ending_sequence.lua" then
+			ComponentSetValue2(comp, "script_source_file", "data/archipelago/scripts/patches/ap_extend_ending.lua")
+		end
 	end
 end
 
