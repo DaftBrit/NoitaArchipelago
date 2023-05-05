b882c76ec40c3e2d997d4bcfb54c33c8aa11f637
Scipio Wright <scipiowright@gmail.com>
2023-05-04 23:33:17 -0400

    Also change so that we don't spam the logger with table is nil errors

--- data/archipelago/scripts/ap_utils.lua
+++ data/archipelago/scripts/ap_utils.lua
@@ -5,10 +5,14 @@ local Log = dofile("data/archipelago/scripts/logger.lua")
 
 
 function contains_element(tbl, elem)
-	for _, v in ipairs(tbl) do
-		if v == elem then return true end
+	if tbl == nil then
+		return false
+	else
+		for _, v in ipairs(tbl) do
+			if v == elem then return true end
+		end
+		return false
 	end
-	return false
 end
 
 
