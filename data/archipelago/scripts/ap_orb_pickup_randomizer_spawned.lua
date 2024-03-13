f6a8d9e87a6ce83d554b2ba29e4e8cc2b62de4ba
Scipio Wright <scipiowright@gmail.com>
2024-03-13 15:51:11 -0400

    Fix issue where you sometimes go over your orb goal amount

--- data/archipelago/scripts/ap_orb_pickup_randomizer_spawned.lua
+++ data/archipelago/scripts/ap_orb_pickup_randomizer_spawned.lua
@@ -3,9 +3,21 @@ dofile_once("data/scripts/lib/utilities.lua")
 
 function item_pickup( entity_item, entity_who_picked, item_name )
 	local pos_x, pos_y = EntityGetTransform( entity_item )
-
 	local message_title = "$itempickup_orb_discovered"
 	local message_desc = "$itempickupdesc_orb_discovered"
+
+	local orb_count = GameGetOrbCountThisRun()
+
+	-- if you reach your goal amount and there are orbs on the ground still, remove their orb components so you don't go over your goal amount
+	if (GameHasFlagRun("ap_peaceful_goal") and orb_count >= 33) or (GameHasFlagRun("ap_pure_goal") and orb_count >= 11) then
+		local existing_orbs = EntityGetWithTag("ap_orb")
+		for _, orb in pairs(existing_orbs) do
+			local orb_comp = EntityGetFirstComponent(orb, "OrbComponent")
+			if orb_comp then
+				EntityRemoveComponent(orb, orb_comp)
+			end
+		end
+	end
 
 	EntityLoad( "data/entities/items/pickup/heart.xml", pos_x, pos_y )
 
