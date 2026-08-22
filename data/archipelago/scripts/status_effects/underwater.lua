5e9884c19c7b666afefa8be0ffd5bb36ce6fc311
Adam Heinermann <aheinerm@gmail.com>
2026-08-22 13:06:53 -0700

    Improved underwater trap sound

--- data/archipelago/scripts/status_effects/underwater.lua
+++ data/archipelago/scripts/status_effects/underwater.lua
@@ -5,5 +5,7 @@ local player = EntityGetRootEntity(GetUpdatedEntityID())
 local dmg_comp = EntityGetFirstComponentIncludingDisabled(player, "DamageModelComponent")
 if dmg_comp == nil then return end
 
--- Forces drowning
+-- Forces drowning and underwater sfx
 ComponentSetValue2(dmg_comp, "mAirDoWeHave", false)
+ComponentSetValue2(dmg_comp, "mAirAreWeInWater", true)
+ComponentSetValue2(dmg_comp, "air_needed", true)
